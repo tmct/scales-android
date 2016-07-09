541b41de3508cb75e1351f3db2c28889907ceb1d
Tom McTiernan <thomasmctiernan@gmail.com>
2016-07-09 14:30:50 +0100

    Create Practice Activity

--- app/src/main/java/uk/co/tmmct/scales/HomeActivity.java
+++ app/src/main/java/uk/co/tmmct/scales/HomeActivity.java
@@ -1,7 +1,9 @@
 package uk.co.tmmct.scales;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void startPractice(View view) {
+        Intent intent = new Intent(this, PracticeActivity.class);
+        startActivity(intent);
     }
 }
