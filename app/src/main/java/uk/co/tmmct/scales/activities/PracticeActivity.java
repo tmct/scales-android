15eb1bcf3a5229176620e53a629a4668fc9904b0
Tom McTiernan <thomasmctiernan@gmail.com>
2016-07-09 17:49:46 +0100

    Add Scale model

--- app/src/main/java/uk/co/tmmct/scales/activities/PracticeActivity.java
+++ app/src/main/java/uk/co/tmmct/scales/activities/PracticeActivity.java
@@ -4,12 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import uk.co.tmmct.scales.R;
+import uk.co.tmmct.scales.models.Scale;
 
 public class PracticeActivity extends AppCompatActivity {
+
+    private Scale[] scales;
+    private int scaleIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+        scales = getScales();
+    }
+
+    private Scale[] getScales() {
+        return new Scale[]{new Scale("C major")};
     }
 }
---
+++ app/src/main/java/uk/co/tmmct/scales/models/scale.java
@@ -0,0 +1,12 @@
+package uk.co.tmmct.scales.models;
+
+/**
+ * Created by thomas on 09/07/2016.
+ */
+public class Scale {
+    public String description;
+
+    public Scale(String description) {
+        this.description = description;
+    }
+}
