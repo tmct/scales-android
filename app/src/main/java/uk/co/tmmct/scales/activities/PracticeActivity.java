3264831c13e929a9dca45d883a0a6e69f70f94f1
Tom McTiernan <thomasmctiernan@gmail.com>
2016-07-10 08:00:05 +0100

    Add first scale databinding

--- app/src/main/java/uk/co/tmmct/scales/activities/PracticeActivity.java
+++ app/src/main/java/uk/co/tmmct/scales/activities/PracticeActivity.java
@@ -1,24 +1,34 @@
 package uk.co.tmmct.scales.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import uk.co.tmmct.scales.R;
+import uk.co.tmmct.scales.databinding.ActivityPracticeBinding;
 import uk.co.tmmct.scales.models.Scale;
 
 public class PracticeActivity extends AppCompatActivity {
 
     private Scale[] scales;
     private int scaleIndex = 0;
+    public Scale scale = new Scale("Chromatic on A");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+        ActivityPracticeBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_practice);
+        binding.setScale(scale);
         scales = getScales();
     }
 
     private Scale[] getScales() {
         return new Scale[]{new Scale("C major")};
+    }
+
+    public void goToNextScale(View view) {
+        //scale.set(new Scale("Contrary motion!"));
     }
 }
